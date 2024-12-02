32fe67639219ea7ac5d91acceae4eeb5c03f82ef
Prajyot Tayde <pmtbmt@gmail.com>
2024-12-02 16:54:56 +0530

    fixing cashfree error

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -235,8 +235,8 @@ export const getSessionId = TryCatch(async(req, res, next) => {
 export const verifyCashfreePayment = TryCatch(async (req, res, next) => {
   const { orderId } = req.body;
 
-  Cashfree.PGOrderFetchPayments("2024-12-02", orderId)
-  // Cashfree.PGOrderFetchPayments("2023-08-01", orderId)
+  // Cashfree.PGOrderFetchPayments("2024-12-02", orderId)
+  Cashfree.PGOrderFetchPayments("2023-08-01", orderId)
       .then((response) => {
           const paymentStatus = response.data;
 
